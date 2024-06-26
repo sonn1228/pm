9d2ce8efdfcb0388dac0b6fed9da968486856cdc
son1228 <sonvipkl04@gmail.com>
2024-06-26 22:54:45 +0700

    nothing

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -1,7 +1,20 @@
 const Product = require('../../models/product.model');
 
+//[GET] /admin/products
+
 module.exports.index = async (req, res) => {
-  const products = await Product.find({});
+
+  // status
+  const find = {
+    deleted: false
+  };
+  if (req.query.status) {
+    find.status = req.query.status;
+  }
+  // end status
+
+
+  const products = await Product.find(find);
   console.log(products);
   res.render('admin/pages/products/index.pug', {
     titlePage: "Product list",
